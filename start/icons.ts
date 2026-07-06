a1f080540adb1e426df54074b22b0d04b0ad3652
NunoF <n.fernandes.contact@gmail.com>
2026-07-06 18:02:18 +0200

    feat(ui): add hero icons and update site header
    Install edge-iconify and @iconify-json/heroicons dependencies
    Configure edge icon setup in start/icons.ts and register the module
    Refine header layout and replace login text links with styled icon button

---
+++ start/icons.ts
@@ -0,0 +1,6 @@
+import edge from 'edge.js'
+import { edgeIconify, addCollection } from 'edge-iconify'
+import { icons as heroIcons } from '@iconify-json/heroicons'
+
+addCollection(heroIcons)
+edge.use(edgeIconify)
